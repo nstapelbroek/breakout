0af3d29cbe1289b85b135275028577b46648540e
Nico Stapelbroek <nwg.stapelbroek@student.han.nl>
2015-05-18 10:59:55 +0200

    created new xcode project

---
+++ Breakout/Breakout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Breakout
+//
+//  Created by Mad Nico on 18/05/15.
+//  Copyright (c) 2015 HAN ICA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
