93d4237f2be6e65169a2fb5fff1539173eaed4ad
Nico Stapelbroek <nwg.stapelbroek@student.han.nl>
2015-05-18 11:29:09 +0200

    add basic gameview based on dropit example

---
+++ Breakout/Breakout/BezierPathsView.swift
@@ -0,0 +1,21 @@
+//
+//  BezierPathsView.swift
+//  Breakout
+//
+//  Created by Mad Nico on 18/05/15.
+//  Copyright (c) 2015 HAN ICA. All rights reserved.
+//
+
+import UIKit
+
+class BezierPathsView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
